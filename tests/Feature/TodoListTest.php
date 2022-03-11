fcf1754e2f76207fb2ee5a67ebd9c5f8396d36bc
Aditya Prasetyo <adty404@gmail.com>
2022-03-11 18:49:57 +0700

    using factory

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -15,12 +15,20 @@ class TodoListTest extends TestCase
     public function fetch_todo_list()
     {
         // preperation / prepare
-        TodoList::create(['name' => 'my list']);
+        TodoList::factory()->create(['name' => 'my list']); //Overiding the default factory
+
+            // example of not overiding the default factory
+            // TodoList::factory()->create();
+            // $data = TodoList::first();
 
         // action / perform
         $response = $this->getJson(route('todo-list.index'));
 
         // assertion / predict
         $this->assertEquals(1,count($response->json()));
+        $this->assertEquals('my list',$response->json()[0]['name']);
+
+            // example of not overiding the default factory
+            // $this->assertEquals($data,$response->json()[0]['name']);
     }
 }
