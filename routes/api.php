1d2204f9a49d693a29710ca72e47f14b785a7ea3
Aditya Prasetyo <adty404@gmail.com>
2022-03-12 13:25:33 +0700

    Todo Task index

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('todo-list', TodoListController::class);
+
+Route::get('task', [TaskController::class,'index'])->name('task.index');
